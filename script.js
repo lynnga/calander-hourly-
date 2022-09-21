c0d7079df40a5ae177357e98e69a28d723c3ccba
lynnga <lingamliel@gmail.com>
2022-09-20 21:51:53 -0500

    added time to javascript and color change

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-var currentDay = moment();
-$("currenrDay").text(today.format("MMM Do, YYYY"));
+
+// // display our current day on the page 
+// document.addEventListener("load",function(){
+
+
+
+
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+var currentHour = moment().hour();
+
+// console.log("hour;" + currentHour);
+var array = document.getElementById("container").querySelectorAll(".row");
+for (let i = 0; i < array.length; i++) {
+    var timeComponent = array[i];
+    var time = 9  + i;
+
+
+    if ( time < currentHour) {
+        // define what the selectors 
+        timeComponent.classList.add('past')
+    } else if (time === currentHour) {
+        timeComponent.classList.remove('past')
+        timeComponent.classList.add('present')
+    } else {
+        timeComponent.classList.remove('past')
+        timeComponent.classList.remove('present')
+        timeComponent.classList.add('future')
+    }
+}
+
+// load any saved datat from our local storage
+// Set Item
+
