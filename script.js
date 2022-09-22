24747c68959eac3a0b590b5186c63652c00cd144
lynnga <lingamliel@gmail.com>
2022-09-22 18:55:48 -0500

    final project completed local storage, need to add readme

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-var timeArray = [9, 10, 11, 12, 1, 2, 3, 4, 5]
-// // display our current day on the page 
+var timeArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+
 $(document).ready(function () {
-    for (let i = 0; i < timeArray.length; i++) {
-        var timeId = timeArray[i];
-    }
-})
+  for (let index = 0; index < timeArray.length; index++) {
+    const elementid = timeArray[index];
+    var timeElement = $("#" + elementid);
 
-
-
+   
+    var textArea = timeElement.siblings()[0]
+   $(textArea).val(localStorage.getItem(elementid));
+    console.log(timeElement.siblings())
+  
+}
+});
 
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
 var currentHour = moment().hour();
 
-// console.log("hour;" + currentHour);
 var array = document.getElementById("container").querySelectorAll(".row");
 for (let i = 0; i < array.length; i++) {
-    var timeComponent = array[i];
-    var time = 9 + i;
+  var timeComponent = array[i];
+  var time = 9 + i;
 
-
-    if (time < currentHour) {
-        // define what the selectors 
-        timeComponent.classList.add('past')
-    } else if (time === currentHour) {
-        timeComponent.classList.remove('past')
-        timeComponent.classList.add('present')
-    } else {
-        timeComponent.classList.remove('past')
-        timeComponent.classList.remove('present')
-        timeComponent.classList.add('future')
-    }
+  if (time < currentHour) {
+    timeComponent.classList.add("past");
+  } else if (time === currentHour) {
+    timeComponent.classList.remove("past");
+    timeComponent.classList.add("present");
+  } else {
+    timeComponent.classList.remove("past");
+    timeComponent.classList.remove("present");
+    timeComponent.classList.add("future");
+  }
 }
 function handleBtnClick(event) {
-    var btn = $(this)
-    var siblings = btn.siblings()
-    var textArea = siblings[1]
-    var textAreaText = textArea.value
-    var timeDivId = siblings[0].getAttribute("id")
-    console.log(textAreaText, timeDivId)
+  var btn = $(this);
+  var siblings = btn.siblings();
+  var textArea = siblings[1];
+  var textAreaText = textArea.value;
+  var timeDivId = siblings[0].getAttribute("id");
+  console.log(textAreaText, timeDivId);
 
-    localStorage.setItem(timeDivId, textAreaText)
-   
-
+  localStorage.setItem(timeDivId, textAreaText);
 }
+$(".btn").on("click", handleBtnClick);
